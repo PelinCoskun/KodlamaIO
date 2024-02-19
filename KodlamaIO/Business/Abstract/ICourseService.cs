f2e9e1dda84efde6775cd0931d416254d13fb8d2
PelinCoskun <pelinefpit@gmail.com>
2024-02-19 19:47:26 +0300

    gs

---
+++ KodlamaIO/Business/Abstract/ICourseService.cs
@@ -0,0 +1,18 @@
+﻿using KodlamaIO.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaIO.Business.Abstract
+{
+    public interface ICourseService
+    {
+        List<Course> GetAll();
+        Course GetById(int id);
+        void Add(Course course);
+        void Update(Course course);
+        void Delete(int id);
+    }
+}
